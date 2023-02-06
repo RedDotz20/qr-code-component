0c8bd63b856efd704b55c35a8c1e3e8555173813
RedDotz20 <carlos_tabangay@outlook.com>
2023-02-06 19:59:11 +0800

    feat: qr container add

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import qrcode from "./assets/image-qr-code.png";
+import "./styles/App.css";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="qr-wrapper">
+			<div className="qr-container">
+				<img src={qrcode} alt="qrcode" />
+				<h3>Improve your front-end skills by building projects</h3>
+				<p>
+					Scan the QR code to visit Frontend Mentor and take your coding skills
+					to the next level
+				</p>
+			</div>
+		</div>
+	);
 }
 
 export default App;
